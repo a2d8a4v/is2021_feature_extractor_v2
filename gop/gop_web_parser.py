f3715c352edde8d164cf61be2bac5ec71783d1a4
a2d8a4v <a2d8a4v@gmail.com>
2022-07-12 00:07:55 -0400

    skip disfluency tokens in tltschool

--- gop/gop_web_parser.py
+++ gop/gop_web_parser.py
@@ -10,7 +10,27 @@ class GOP(object):
         self.phn_dur_fn = conf.get("phn-dur", None)
         self.lexicon = conf.get("lexicon", "IPA")
         self.lang_dir = conf.get("lang-dir", None)
+        self.skip_word_unk = conf.get('skip-word-unk', True)
         self.silence_phones = conf.get("silence-phones", "0:1:2:3:4:5")
+        self.special_tokens = ['<unk>', '<UNK>']
+        self.s2t = conf.get('s2t', False)
+        self.silence_tokens_list = self._open_silence_tokens(
+                os.path.abspath(
+                    os.path.join(self.dir_name, conf.get('sil-phone-path'))
+                )
+            ) if conf.get('sil-phone-path', None) is not None else []
+        self.disfluency_special_tokens = self._open_disfluency_tokens(
+                os.path.abspath(
+                    os.path.join(self.dir_name, conf.get('nonsil-phone-path'))
+                )
+            ) if conf.get('nonsil-phone-path', None) is not None else []
+        if self.s2t:
+            self.s2t_skip_word_unk = conf.get('s2t-skip-word-unk', True)
+            self.disfluency_special_tokens = self._open_disfluency_tokens(
+                    os.path.abspath(
+                        os.path.join(self.dir_name, conf.get('nonsil-phone-path'))
+                    )
+                ) if conf.get('nonsil-phone-path', None) is not None else []
 
         # load words.txt for oov handler
         vocab_file = open(self.dir_name + "/" + self.lang_dir + "/words.txt", "r")
@@ -33,6 +53,32 @@ class GOP(object):
         # silence phones
         self.silence_phones = self.silence_phones.split(":")
     
+    @classmethod
+    def _open_disfluency_tokens(self, file_path):
+        filter_list = ['@', '<']
+        disfluency_list = []
+        with open(file_path, 'r') as f:
+            for line in f.readlines():
+                for filter in filter_list:
+                    token = line.split()[0]
+                    if filter == token[0]:
+                        disfluency_list.append(token)
+        return disfluency_list
+
+    @classmethod
+    def _open_silence_tokens(self, file_path):
+        # filter_list = ['sil', 'spn', 'hes', 'unk']
+        silence_set = set()
+        with open(file_path, 'r') as f:
+            for line in f.readlines():
+                token = line.strip()
+                # if token in filter_list:
+                silence_set.add(token)
+                token = line.strip().split('_')[0]
+                # if token in filter_list:
+                silence_set.add(token)
+        return list(silence_set)
+
     def sigmoid(self, x, alpha = 1, c = 0):
         # score function
         return 1. / (1 + np.exp(-1 * alpha * (x-c)))
@@ -60,8 +106,21 @@ class GOP(object):
         j = 0
         for i in range(len(prompt_list)):
             word = prompt_list[i]
-            if word.upper() == '<UNK>':
-                continue
+            if self.skip_word_unk and not self.s2t:
+                if word in self.disfluency_special_tokens:
+                    continue
+                if word in self.special_tokens:
+                    continue
+            
+            if self.s2t:
+                if self.s2t_skip_word_unk:
+                    if word in self.disfluency_special_tokens:
+                        continue
+                    if word in self.special_tokens:
+                        continue
+
+            # print(word)
+
             phone_list = []
             stress_list = []
             sound_list = []
@@ -71,9 +130,19 @@ class GOP(object):
             # NOTE: We'd like to set word-position-dependency=true now (That's not necessary)
             while True:
                 # get the GOP_msg of server
+                # try:
                 phone = gop_list[j]
+                    # print("{} - {} - {}".format(word, j , phone))
+                # except:
+                #     assert 1==2, "{} - {} - {} - {}".format(j, phone, gop_list, prompt_list)
                 pred_phone = gop_list[j+1]
                 duration = float(gop_list[j+2])
+
+                # -2. skip the silence tokens
+                if re.sub("\d+", '', phone.lower().split('_')[0]) in self.silence_tokens_list:
+                    j+=4
+                    continue
+
                 # -1. silence detection (silence or spoken noise)
                 is_silence = (self.phone_table[pred_phone] in self.silence_phones)
                 # 0. GOP score
